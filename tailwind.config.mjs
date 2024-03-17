4a157f50113b5bc3b425753b3942645fa1cb8b31
Maxime Grisole <grisole.maxime@gmail.com>
2024-03-17 14:12:15 +0100

    first-commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors: {
+				'baby-powder': '#f8f7f2',
+				'old-rose': '#c2747d',
+				'independence': '#3d405b',
+				'black-olive': '#353d39',
+				'almond': '#E7D4C4'
+			},
+			fontFamily: {
+				'brand': ['Quicksand Variable', ...defaultTheme.fontFamily.sans]
+			},
+			screens: {
+				// TODO: Switch to em
+				sm: { min: '680px' },
+				md: { min: '1920px' },
+				lg: { min: '2048px' }
+			}
+		},
+	},
+	plugins: [],
+}
