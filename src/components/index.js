20aea06fe86d71c18fa235d3edb2a5fbde645d64
Maxime Grisole <grisole.maxime@gmail.com>
2024-03-31 20:50:08 +0200

    export components from barrel

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 export {default as Modal} from './modal.astro'
+export {default as List} from './list.astro'
+export {default as Slider} from './slider/slider.astro'
+export {default as Chapter} from './chapter.astro'
+export {MoreInfo} from './moreInfo.tsx'
