7f46a3707a610a684eb41c0ef7a9d82b19457801
Maxime Grisole <grisole.maxime@gmail.com>
2024-04-01 00:42:49 +0200

    wip

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export {default as Modal} from './modal.astro'
-export {default as List} from './list.astro'
-export {default as Slider} from './slider/slider.astro'
-export {default as Chapter} from './chapter.astro'
-export {MoreInfo} from './moreInfo.tsx'
+export { default as Modal } from "./modal.astro";
+export { default as List } from "./list.astro";
+export { default as Slider } from "./slider/slider.astro";
+export { default as Chapter } from "./chapter.astro";
+export { default as CenteredBlock } from "./centeredBlock.astro";
+export { MoreInfo } from "./moreInfo.tsx";
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,20 +1,22 @@
-import type {Component, JSX} from "solid-js";
+import type { Component, JSX } from "solid-js";
 import { BiRegularChevronLeft, BiRegularChevronRight } from "solid-icons/bi";
 
-export const Slider: Component<{children?: JSX.Element | JSX.Element[]}> = ({children}) => {
+export const Slider: Component<{ children?: JSX.Element | JSX.Element[] }> = ({
+  children,
+}) => {
   return (
     <div class="blaze-slider flex">
-      <button aria-label="précédent" class="blaze-prev"><BiRegularChevronLeft class="text-3xl cursor-pointer" /></button>
+      <button aria-label="précédent" class="blaze-prev">
+        <BiRegularChevronLeft class="text-3xl cursor-pointer" />
+      </button>
       <div class="blaze-container flex-1">
-
         <div class="blaze-track-container">
-          <div class="blaze-track">
-            {children}
-          </div>
+          <div class="blaze-track">{children}</div>
         </div>
-
       </div>
-        <button  aria-label="suivant" class="blaze-next"><BiRegularChevronRight class="text-3xl cursor-pointer" /></button>
+      <button aria-label="suivant" class="blaze-next">
+        <BiRegularChevronRight class="text-3xl cursor-pointer" />
+      </button>
     </div>
   );
 };
