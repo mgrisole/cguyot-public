c65cb5653c77b19dd7ab21f56addd199500a3986
Maxime Grisole <grisole.maxime@gmail.com>
2024-04-01 21:33:34 +0200

    update astro config site attribute

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import tunnel from "astro-tunnel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mgrisole.github.io/',
-  base: '/cguyot-public',
+  site: 'https://www.caroline-guyot.com/',
   devToolbar: {
     enabled: true
   },
